30ef4942a96238a48be4fa8dbcc8a520805a543b
stefanysansouza <stefany.san.souza@gmail.com>
2026-03-27 22:31:06 -0300

    Update configuracoes.php

---
+++ configuracoes.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Configurações</title>
+<link rel="stylesheet" href="css/dash.css">
+</head>
+<body>
+<h1>Configurações</h1>
+<p>Ajustes</p>
+</body>
+</html>
