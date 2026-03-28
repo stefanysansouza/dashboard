1d8e8be55a88f9ce06331e0b42c5a18bf1b3e513
stefanysansouza <stefany.san.souza@gmail.com>
2026-03-27 22:31:46 -0300

    Update relatorios.php

---
+++ relatorios.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Relatórios</title>
+<link rel="stylesheet" href="css/dash.css">
+</head>
+<body>
+<h1>Relatórios</h1>
+<p>Dados do sistema</p>
+</body>
+</html>
